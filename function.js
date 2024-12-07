e8df49dcfa4fa1378f4c553067fd6b4fa373cb1a
niā <niassu@protonmail.com>
2024-12-06 22:45:14 -0500

    Revised function.

--- function.js
+++ function.js
@@ -1,8 +1,8 @@
 const now = new Date();
-const st = new Date(now.getFullYear(), 0,0);
+const st = new Date(Date.UTC(now.getFullYear(),0,0));
 const dy = 24*3600000;
 
-function yearDay() {
-return Math.floor((now-st)/dy);
+function yearDay(){
+  return Math.floor((now-st)/dy);
 }
-// there is no handling for DST
+// only works based off of UTC time, not adjusted for timezones.
